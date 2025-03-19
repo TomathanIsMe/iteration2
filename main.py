6705a51e3c89e001fec6ec8651041cf92c8d4884
Tom Croezen <yoghurtdrinker@gmail.com>
2025-03-19 19:51:01 +0100

    yop

--- main.py
+++ main.py
@@ -34,10 +34,28 @@ class Audio:
         self.CHUNK = 1024
         self.RECORD_SECONDS = 5
 
-    def play_audio(self):
+    def play_audio_introduction(self):
         global goblinstate
         goblinstate = "speaking"
         wf = wave.open("introduction.wav", 'rb')
+
+        audio = pyaudio.PyAudio()
+        stream = audio.open(format=audio.get_format_from_width(wf.getsampwidth()),
+                            channels=wf.getnchannels(),
+                            rate=wf.getframerate(),
+                            output=True)
+        data = wf.readframes(self.chunk)
+        while data != b'':
+            stream.write(data)
+            data = wf.readframes(self.chunk)
+        stream.close()
+        audio.terminate()
+        goblinstate = "idle"
+        return
+    def play_audio_riddle(self):
+        global goblinstate
+        goblinstate = "speaking"
+        wf = wave.open("riddle.wav", 'rb')
 
         audio = pyaudio.PyAudio()
         stream = audio.open(format=audio.get_format_from_width(wf.getsampwidth()),
@@ -163,7 +181,16 @@ def interfaceboot():
         height=5,
         bg="white",
         fg="black",
-        command=audiocall.play_audio
+        command=audiocall.play_audio_introduction
+    )
+    button.pack()
+    button = tk.Button(
+        text="play riddle",
+        width=25,
+        height=5,
+        bg="white",
+        fg="black",
+        command=audiocall.play_audio_riddle
     )
     button.pack()
 
